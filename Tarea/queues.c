248eb742219adb2e5b581eff5f46b9190721de3a
Manuel Gomes <manuelgomesg@gmail.com>
2017-01-29 19:13:58 -0400

    terminadas las funciones que tienen algo de sentido

--- Tarea/queues.c
+++ Tarea/queues.c
@@ -95,8 +95,81 @@ void ElimProcesoE(EstrucSched *s) {
   o como salen de ejecucion los procesos*/
 }
 
+void eliminarProcs(Proceso *p) {
+  if (p -> next != NULL)
+    eliminarProcs(p -> next);
+  destruirProceso(p);
+}
 
+
+void eliminarProcesosCola(Cola *q)  {
+  eliminarProcs(q -> first);
+  free(q);
+}
+
+
+Proceso *ProxProceso(EstrucSched *s) {
+  if (s -> q0 -> first != NULL) {
+    return s -> q0 -> first;
+  }
+  else if (s -> q1 -> first != NULL) {
+    return s -> q1 -> first;
+  }
+  else if (s -> q2 -> first != NULL) {
+    return s -> q2 -> first;
+  }
+  else if (s -> q3 -> first != NULL) {
+    return s -> q3 -> first;
+  }
+  else if (s -> q4 -> first != NULL) {
+    return s -> q4 -> first;
+  }
+  else if (s -> q5 -> first != NULL) {
+    return s -> q5 -> first;
+  }
+}
+
+
+
+void CambiarEstado(EstrucSched *s, Proceso* p, char newestado) {
+  p -> status = newestado;
+  /* Si me dan un apuntador al proceso no necesito la estructura o las colas*/
+}
 
 EstrucSched *Construye(char *filename) {
 
+}
+
+void imprimecola(Cola * q) {
+  if (q -> first == NULL) {
+    return;
+  }
+  else {
+    Proceso *aux = q -> first;
+    printf("(");
+    printf(" %d %c %d %d %s, \n", aux -> pid, aux -> status, aux -> prio,
+                                      aux -> time, aux -> command);
+    while (aux -> next != NULL) {
+      aux = aux -> next;
+      printf(" %d %c %d %d %s, \n", aux -> pid, aux -> status, aux -> prio,
+                                        aux -> time, aux -> command);
+    }
+    printf(")\n");
+  }
+}
+
+
+void Imprime(EstrucSched *s) {
+  printf("Cola q0:  \n" );
+  imprimecola(q0);
+  printf("Cola q1:  \n" );
+  imprimecola(q1);
+  printf("Cola q2:  \n" );
+  imprimecola(q2);
+  printf("Cola q3:  \n" );
+  imprimecola(q3);
+  printf("Cola q4:  \n" );
+  imprimecola(q4);
+  printf("Cola q5:  \n" );
+  imprimecola(q5);
 }
